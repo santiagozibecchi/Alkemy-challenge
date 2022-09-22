396c8d7b6baf30d0877ca63a6dea58d1293a227f
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-22 18:34:18 -0300

    form structure

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = () => {
+   return (
+      <form>
+         <input type="email" name="email" />
+         <input type="password" name="password" />
+
+         <button type="submit">Ingresar</button>
+      </form>
+   );
+};
+
+export default Login;
