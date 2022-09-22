346cb2f248cb94ef60e487c7489691e16a2b708d
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-22 19:21:42 -0300

    formulario y validacion

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,60 @@
-import React from "react";
+import { useState } from "react";
 
 const Login = () => {
-   return (
-      <form>
-         <input type="email" name="email" />
-         <input type="password" name="password" />
+   const [formValues, setFormValues] = useState({
+      email: "",
+      password: "",
+   });
 
-         <button type="submit">Ingresar</button>
-      </form>
+   const handleSubmit = (e) => {
+      e.preventDefault();
+
+      const regexEmail =
+         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      if (formValues.email === "" || formValues.password) {
+         return console.log("Los campos no pueden estar vacios");
+      }
+
+      if (!regexEmail.test(formValues.email)) {
+         return console.log("Email no valido");
+      }
+
+   };
+
+   const handleOnChange = (e) => {
+      setFormValues({ ...formValues, [e.target.name]: e.target.value });
+   };
+
+   return (
+      <>
+         <h2>Formulario de Login</h2>
+         <form onSubmit={handleSubmit}>
+            <div>
+               <label htmlFor="email">Email</label>
+               <input
+                  id="email"
+                  type="text"
+                  name="email"
+                  value={formValues.email}
+                  onChange={handleOnChange}
+               />
+            </div>
+
+            <div>
+               <label htmlFor="password">Contraseña</label>
+               <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  value={formValues.password}
+                  onChange={handleOnChange}
+               />
+            </div>
+
+            <button type="submit">Ingresar</button>
+         </form>
+      </>
    );
 };
 
