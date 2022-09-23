3366b4323b607fab458f24c10bd271518b384beb
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-23 16:34:43 -0300

    agregando algunos estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import Login from "./components/Login";
 import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 import ListApp from "./components/ListApp";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+// Styles
+import "./css/bootstrap.min.css";
+
 const App = () => {
    return (
-      <>
+      <div className="container">
          <Header />
          <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/listado" element={<ListApp />} />
          </Routes>
          <Footer />
-      </>
+      </div>
    );
 };
 
