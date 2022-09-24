8fdd879b3aa64afcfdba7acdfb0007bfe619b2cb
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-24 09:46:42 -0300

    cambio en el estilo global app  + boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 // Styles
-import "./css/App.css";
+import styles from "./css/App.module.css";
 
 const App = () => {
    return (
       <div>
          <Header />
-         <div className="container">
+         <div className={styles.container}>
             <Routes>
                <Route path="/" element={<Login />} />
                <Route path="/listado" element={<ListApp />} />
